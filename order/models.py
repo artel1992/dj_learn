616caf9bcbdaacad5e6ab7fd6dee7ce81f47ed18
Artem <artela1992@gmail.com>
2020-10-16 09:18:28 +0300

    виртуал

--- order/models.py
+++ order/models.py
@@ -4,5 +4,6 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     user = models.ForeignKey('acount.User', on_delete=models.CASCADE)
-    product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
+    product = models.ManyToManyField('product.Product')
     color = models.ForeignKey('product.Color', on_delete=models.CASCADE)
+    sum_price = models.FloatField(default=0)
